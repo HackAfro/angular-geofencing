402bffbb9f7d3025b0b526ea532c4b2ab3148e16
Richard Afro <richyafro@hotmail.com>
2018-05-13 08:59:35 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { AgmCoreModule } from '@agm/core';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PusherService } from './pusher.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      // please get your own API key here:
+      // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
+      apiKey: 'AIzaSyB9cHV0JyIRbDrktHDput56nq0EDQLmwgE',
+      libraries: ['geometry']
+    }),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PusherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
